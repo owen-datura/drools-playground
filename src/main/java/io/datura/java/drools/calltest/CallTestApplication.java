6e47a03dcde796eb2a21e48e3dd68a447329da83
Owen Searfoss <owen@datura.io>
2018-11-26 14:19:06 -0600

    Initial commit

---
+++ src/main/java/io/datura/java/drools/calltest/CallTestApplication.java
@@ -0,0 +1,12 @@
+package io.datura.java.drools.calltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallTestApplication.class, args);
+	}
+}
