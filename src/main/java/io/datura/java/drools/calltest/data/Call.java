710968445abc5207c12b245e880f71c9c61db568
Owen Searfoss <owen@datura.io>
2018-11-27 11:34:16 -0600

    Added random localdate generator

--- src/main/java/io/datura/java/drools/calltest/data/Call.java
+++ src/main/java/io/datura/java/drools/calltest/data/Call.java
@@ -1,16 +1,18 @@
 package io.datura.java.drools.calltest.data;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Random;
 
 public class Call {
 	private LocalDateTime callTime;
 	private CallerDemographics callerDemo;
 	private CallerProducts products;
-	
+
 	public Call() {
 		this(null);
 	}
-	
+
 	public Call(LocalDateTime callTime) {
 		this.callTime = callTime != null ? callTime : LocalDateTime.now();
 		callerDemo = new CallerDemographics();
@@ -27,5 +29,17 @@ public class Call {
 
 	public CallerProducts getProducts() {
 		return products;
+	}
+
+	public static LocalDateTime getRandomizedTime() {
+		Random r = new Random();
+		int dateOffset = r.nextInt(10);
+		int hourOffset = r.nextInt(23);
+		
+		LocalDateTime rdt = LocalDateTime.now();
+		rdt = rdt.minus(dateOffset, ChronoUnit.DAYS);
+		rdt = rdt.minus(hourOffset, ChronoUnit.HOURS);
+		
+		return rdt;
 	}
 }
--- src/test/java/io/datura/java/drools/calltest/data/CallTest.java
+++ src/test/java/io/datura/java/drools/calltest/data/CallTest.java
@@ -2,6 +2,8 @@ package io.datura.java.drools.calltest.data;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+
 import org.junit.Test;
 
 public class CallTest {
@@ -10,5 +12,17 @@ public class CallTest {
 	public void testCallDateIsPopulated() {
 		Call c = new Call();
 		assertNotNull(c.getCallTime());
+	}
+	
+	@Test
+	public void testRandomDateGeneration() {
+		LocalDateTime ldt = Call.getRandomizedTime();
+		assertNotNull(ldt);
+	}
+	
+	@Test
+	public void testRandomDateIsInPast() {
+		LocalDateTime ldt = Call.getRandomizedTime();
+		assertTrue(LocalDateTime.now().isAfter(ldt));
 	}
 }
