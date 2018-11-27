deb4717107ed977f3a91a91acafdd7a0c7c7ad02
Owen Searfoss <owen@datura.io>
2018-11-27 14:26:52 -0600

    Latest

--- src/main/java/io/datura/java/drools/calltest/controller/CallWebController.java
+++ src/main/java/io/datura/java/drools/calltest/controller/CallWebController.java
@@ -1,14 +1,21 @@
 package io.datura.java.drools.calltest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import io.datura.java.drools.calltest.service.CallDAOService;
+
 @Controller
 public class CallWebController {
 
+	@Autowired
+	private CallDAOService callService;
+	
 	@GetMapping("/")
 	public String getLandingPage( Model model ) {
+		model.addAttribute("calls", callService.getAllCalls());
 		return "index";
 	}
 }
