8bad1c9b91fb7209e3762de4a758f1cb3e5805da
Owen Searfoss <owen@datura.io>
2018-11-28 15:17:53 -0600

    Fix JSHint errors, restructured inside IIFE

--- src/main/resources/static/js/calls.js
+++ src/main/resources/static/js/calls.js
@@ -1,49 +1,41 @@
-"use strict"
+/* jshint esversion: 6 */
+/* globals Vue:false */
 
-const app = new Vue({
-	el: '#app',
-	data: {
-		calls: []
-	},
-	created() {
-		fetch('/calls')
-		  .then(response => response.json())
-		  .then(json => {
-			  this.calls = json;
-		  })
-	}
-});
+(function() {
+	'use strict';
 
-document.getElementById('btnCallReset').addEventListener('click', event => {
-	event.preventDefault();
-	fetch('/calls/reset', {
-		method: 'post'
-	})
-	.then( response => response.json())
-	.then( json => {
-		app.calls = json;
+	const app = new Vue({
+		el: '#app',
+		data: {
+			calls: []
+		},
+		created() {
+			fetch('/calls')
+			  .then(response => response.json())
+			  .then(json => {
+				  this.calls = json;
+			  });
+		}
 	});
-});
 
-function filterCallingRecords(filterId) {
-	let url = '/calls/filterByRuleSet/' + filterId;
-	fetch(url, {
-		method: 'post'
-	})
-	.then( response => response.json())
-	.then( json => {
-		app.calls = json;
+	document.getElementById('btnRunFilter').addEventListener('click', event => {
+		event.preventDefault();
+		fetch('/calls/filterByRuleSet', {
+			method: 'post'
+		}).then( response => response.json())
+		.then( json => {
+			app.calls = json;
+		});
 	});
-};
 
-document.getElementById('btnFilterLgCo').addEventListener('click', event => {
-	event.preventDefault();
-	const filterId = event.target.dataset.filterid;
-	filterCallingRecords(filterId);
-});
-
-document.getElementById('btnFilterAmOnPrem').addEventListener('click', event => {
-	event.preventDefault();
-	const filterId = event.target.dataset.filterid;
-	filterCallingRecords(filterId);
-});
+	document.getElementById('btnCallReset').addEventListener('click', event => {
+		event.preventDefault();
+		fetch('/calls/reset', {
+			method: 'post'
+		})
+		.then( response => response.json())
+		.then( json => {
+			app.calls = json;
+		});
+	});
+})();
